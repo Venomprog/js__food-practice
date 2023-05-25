59d04c82318afb351cd908d0c76ffa0912677639
Gleb <glebshaevvv@mail.ru>
2023-05-25 08:11:40 +0300

    update

--- js/script.js
+++ js/script.js
@@ -36,4 +36,41 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  function myAnimation(){
+    const elem = document.querySelector('.element');
+
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+
+    function frame () {
+      if (pos === 300){
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+      }
+    }
+  }
+
+  // const btn = document.querySelector('.calculating__choose-item');
+
+  // let timerID,
+  //     i = 0;
+
+  // btn.addEventListener('click', () => {
+  //   timerID = setInterval(logger, 500);
+  // });
+
+  // function logger () {
+  //   if (i === 3){
+  //     clearInterval(timerID);
+  //   }
+
+  //   console.log('text');
+  //   i++;
+  // }
+
 });
